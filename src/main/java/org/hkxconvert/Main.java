44d4bf2ff493d55c28c3c9b385da544ea0ecb2a4
Tianyun Yuan <7ianyun@berkeley.edu>
2021-10-29 02:27:22 -0700

    add hvy attack connector

--- src/main/java/org/hkxconvert/Main.java
+++ src/main/java/org/hkxconvert/Main.java
@@ -2,19 +2,36 @@ package org.hkxconvert;
 
 import org.hkxconvert.manager.EnemyBrutalizer;
 import org.hkxconvert.manager.FixManager;
+import org.hkxconvert.manager.HvyComboInitiater;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import static org.hkxconvert.Const.PATH;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose your annotator");
+        System.out.println("L for light attack connector");
+        System.out.println("H for heavy attack connector");
         File root = new File(PATH);
         List<FilePath> filePaths = new ArrayList<>();
-        FixManager fixManager = new EnemyBrutalizer(root, filePaths);
+        FixManager fixManager;
+        while (true) {
+            if (input.next().toLowerCase(Locale.ROOT).equals("l")) {
+                System.out.println("annotating light combos");
+                fixManager = new EnemyBrutalizer(root, filePaths);
+                break;
+            } else if (input.next().toLowerCase(Locale.ROOT).equals("H")) {
+                System.out.println("annotating light to heavy combos");
+                fixManager = new HvyComboInitiater(root, filePaths);
+            }
+        }
         fixManager.dumpAnno();
         fixManager.fixAnno();
         fixManager.updateAnno();
---
+++ src/main/java/org/hkxconvert/manager/HvyComboInitiater.java
@@ -0,0 +1,18 @@
+package org.hkxconvert.manager;
+
+import org.hkxconvert.FilePath;
+
+import java.io.File;
+import java.util.List;
+
+public class HvyComboInitiater extends FixManager {
+
+    public HvyComboInitiater(File root, List<FilePath> filePaths) {
+        super(root, filePaths);
+    }
+
+    @Override
+    public void fixAnno() {
+
+    }
+}

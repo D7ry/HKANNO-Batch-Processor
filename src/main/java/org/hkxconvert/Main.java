29e41aeee51d57bae814b406841521af5f176891
d7ry <7ianyun@berkeley.edu>
2021-10-31 03:02:48 -0700

    fix typo

--- src/main/java/org/hkxconvert/Main.java
+++ src/main/java/org/hkxconvert/Main.java
@@ -49,7 +49,7 @@ public class Main {
                     System.out.println("H for heavy attack combo chaining");
                     System.out.println("C for heavy attack looper(incomplete)");
                     System.out.println("RM to remove all annotations with keyword");
-                    System.out.println("RP to replace all anotations");
+                    System.out.println("RP to replace all annotations");
                     System.out.println("D to dump all annotations");
                     System.out.println("U to update all annotations with corresponding names");
                 }
